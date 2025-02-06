ad65b5483fcc871805de0561951bc257f99184e0
Goni Schindler <goni.schindler@metropark.co.il>
2025-02-06 19:22:37 +0200

    theme

---
+++ app/src/main/java/il/co/gonisch/moviesphere/ui/theme/Theme.kt
@@ -0,0 +1,49 @@
+package il.co.gonisch.moviesphere.ui.theme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+
+private val customColorScheme = darkColorScheme(
+    primary = primaryDark,
+    onPrimary = onPrimaryDark,
+    primaryContainer = primaryContainerDark,
+    onPrimaryContainer = onPrimaryContainerDark,
+    secondary = secondaryDark,
+    onSecondary = onSecondaryDark,
+    secondaryContainer = secondaryContainerDark,
+    onSecondaryContainer = onSecondaryContainerDark,
+    tertiary = tertiaryDark,
+    onTertiary = onTertiaryDark,
+    tertiaryContainer = tertiaryContainerDark,
+    onTertiaryContainer = onTertiaryContainerDark,
+    error = errorDark,
+    onError = onErrorDark,
+    errorContainer = errorContainerDark,
+    onErrorContainer = onErrorContainerDark,
+    background = backgroundDark,
+    onBackground = onBackgroundDark,
+    surface = surfaceDark,
+    onSurface = onSurfaceDark,
+    surfaceVariant = surfaceVariantDark,
+    onSurfaceVariant = onSurfaceVariantDark,
+    outline = outlineDark,
+    outlineVariant = outlineVariantDark,
+    scrim = scrimDark,
+    inverseSurface = inverseSurfaceDark,
+    inverseOnSurface = inverseOnSurfaceDark,
+    inversePrimary = inversePrimaryDark,
+)
+
+
+@Composable
+fun MovieSphereTheme(
+    content: @Composable() () -> Unit
+) {
+
+  MaterialTheme(
+    colorScheme = customColorScheme,
+    typography = MaterialTheme.typography,
+    content = content
+  )
+}
+
---
+++ app/src/main/java/il/co/gonisch/moviesphere/ui/theme/Type.kt
@@ -0,0 +1,3 @@
+package il.co.gonisch.moviesphere.ui.theme
+
+import androidx.compose.material3.Typography
